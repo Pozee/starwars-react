56e52d432cffb1b11efb9c87ee8d1f6a7d9913bd
Pozee <kontakt@pontuszetterberg.com>
2020-06-03 11:46:38 +0200

    Updates

---
+++ precache-manifest.cf5da6831da5aea21dab70834323561d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdf7a98b871574a4d854ee7c4a9306b",
+    "url": "/starwars-react/index.html"
+  },
+  {
+    "revision": "e6c5a19f2d463e444a3e",
+    "url": "/starwars-react/static/css/main.b49a611e.chunk.css"
+  },
+  {
+    "revision": "87a90426de93bedc9507",
+    "url": "/starwars-react/static/js/2.2297770d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars-react/static/js/2.2297770d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c5a19f2d463e444a3e",
+    "url": "/starwars-react/static/js/main.aed437af.chunk.js"
+  },
+  {
+    "revision": "4a9703631fa8aa83b9c0",
+    "url": "/starwars-react/static/js/runtime-main.11ae47e3.js"
+  }
+]);
